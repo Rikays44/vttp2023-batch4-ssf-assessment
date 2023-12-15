6e71e7920629dbfc614bcef8258eb443f7d29479
Rikays44 <Rikays44@gmail.com>
2023-12-15 15:03:18 +0800

    update

--- vttp2023-batch4-ssf-assessment/src/main/java/vttp/ssf/assessment/eventmanagement/controllers/EventController.java
+++ vttp2023-batch4-ssf-assessment/src/main/java/vttp/ssf/assessment/eventmanagement/controllers/EventController.java
@@ -1,14 +1,29 @@
 package vttp.ssf.assessment.eventmanagement.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import vttp.ssf.assessment.eventmanagement.model.user;
+
 @Controller
-@RequestMapping("/")
+@RequestMapping("/home")
 public class EventController {
 
+	@GetMapping("/first")
+	public String displayEvents(){
+
+		return "view0";
+	}
+  @PostMapping("/second") {
+public String register(Model model){
 	
-	//TODO: Task 5
+return "view1";
+}
+  }
+	
 
 
 }
